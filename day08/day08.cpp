1fc93c24e38a9f94cad8e1cbab48f34c8b36ee39
Chaitanya Kukde <ckukde@gmail.com>
2024-12-22 21:10:59 +0000

    Day08 Part 1

---
+++ day08/day08.cpp
@@ -0,0 +1,60 @@
+//
+// Created by chaku on 21/12/24.
+//
+
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+#include <ranges>
+
+#include "day08.hpp"
+
+auto test() -> int {
+    const std::vector<std::string> test_input{
+            "............",
+            "........0...",
+            ".....0......",
+            ".......0....",
+            "....0.......",
+            "......A.....",
+            "............",
+            "............",
+            "........A...",
+            ".........A..",
+            "............",
+            "............"
+    };
+    assert(aoc::day08::get_num_unique_locations(test_input) == 14);
+    return 0;
+}
+
+auto parse_and_run(std::string_view path) -> int {
+    std::vector<std::string> data;
+    std::fstream file(static_cast<std::string>(path));
+    if (!file.is_open()) {
+        std::cerr << "Failed to open " << std::quoted(path) << "\n";
+        return 1;
+    }
+    std::vector<std::string> input;
+    std::string line;
+    while(std::getline(file, line)) {
+        input.emplace_back(line);
+    }
+    std::cout << "Part 1 solution : " << aoc::day08::get_num_unique_locations(input) << "\n";
+    //std::cout << "Part 2 solution : " << aoc::day07::calibration_result_if_true(input, true) << "\n";
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        return test();
+    }
+    else if (argc == 2) {
+        return parse_and_run(argv[1]);
+    }
+    return 0;
+}
+
---
+++ day08/day08.hpp
@@ -0,0 +1,130 @@
+//
+// Created by chaku on 14/12/24.
+//
+
+#ifndef AOC2024_DAY08_HPP
+#define AOC2024_DAY08_HPP
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <cmath>
+
+
+namespace aoc::day08 {
+    [[maybe_unused]] auto print (const auto& container) {
+        size_t index{0};
+        const size_t sz{container.size()};
+        std::for_each(container.cbegin(), container.cend(), [&] (const auto& elem) {
+            std::cout << elem ;
+            if (index != sz-1)
+            {
+                std::cout << ", ";
+                index++;
+            }
+        });
+        std::cout << "\n";
+    };
+
+    struct LocationPair {
+        int r;
+        int c;
+
+        bool operator==(const LocationPair& other) const {
+            return other.r == r and other.c == c;
+        }
+    };
+
+    struct LocationPairHash {
+        size_t operator()(const LocationPair& location) const
+        {
+            auto h1 = std::hash<int>{}(location.r);
+            auto h2 = std::hash<int>{}(location.c);
+            return h1 ^ (h2 << 1);
+        }
+    };
+    using LocationPairSet = std::unordered_set<LocationPair, LocationPairHash>;
+    using LocationPairVec = std::vector<LocationPair>;
+    using AntennaLocationMap = std::unordered_map<char, LocationPairVec>;
+
+    auto get_antenna_locations(const auto& input) -> AntennaLocationMap {
+        const auto num_rows = input.size();
+        const auto num_cols = input[0].size();
+        AntennaLocationMap antennas;
+        for (int r = 0; r < num_rows; ++r) {
+            for (int c = 0; c < num_cols; ++c) {
+                if (input[r][c] == '.') {
+                    continue;
+                }
+                char antenna = input[r][c];
+                if (antennas.contains(antenna)) {
+                    antennas[antenna].emplace_back(LocationPair(r, c));
+                    continue;
+                }
+                antennas[antenna] = {{r, c}};
+            }
+        }
+        return antennas;
+    }
+
+    auto get_distance(const int pc, const int pr, const int ic, const int ir) {
+        int distance{0};
+        distance = std::abs(ic - pc)  + std::abs(ir - pr);
+        return distance;
+    }
+
+    auto get_num_unique_locations(const auto& input) {
+        LocationPairSet result;
+        const auto& antenna_locations = get_antenna_locations(input);
+        const auto num_rows = input.size();
+        const auto num_cols = input[0].size();
+        for (int r = 0; r < num_rows; ++r) {
+            for (int c = 0; c < num_cols; ++c) {
+                for (const auto& [key, value] : antenna_locations) {
+                    for (int i = 0; i < value.size(); ++i) {
+                        for (int j = i+1; j < value.size(); ++j) {
+                            const auto& antenna1 = value[i];
+                            const auto& antenna2 = value[j];
+                            //if (antenna1.r == antenna2.r and antenna1.c == antenna2.c) {
+                            //    continue;
+                            //}
+                            const auto distance_point_antenna1 = get_distance(c, r, antenna1.c, antenna1.r);
+                            const auto distance_point_antenna2 = get_distance(c, r, antenna2.c, antenna2.r);
+                            //const auto slope_point_antenna1 = get_slope(c, r, antenna1.c, antenna1.r);
+
+                            //const auto slope_point_antenna2 = get_slope(c, r, antenna2.c, antenna2.r);
+
+                            constexpr auto twice_distance = [](const double p_to_ant1, const double p_to_ant2) -> bool {
+                                return (p_to_ant1 == 2 * p_to_ant2) or (p_to_ant2 == 2 * p_to_ant1);
+                            };
+
+                            const auto in_one_line = [&r, &c](const LocationPair& ant1, const LocationPair& ant2) -> bool {
+                                // point to antenna1 = (r - ant1.r)/(c - ant1.c)
+                                // point to antenna2 = (r - ant2.r)/(c - ant2.c)
+                                return ((r - ant1.r) * (c - ant2.c)) == ((c - ant1.c) * (r - ant2.r));
+                            };
+
+
+                            if (in_one_line(antenna1, antenna2) and
+                            twice_distance(distance_point_antenna1,
+                                           distance_point_antenna2))
+                            {
+                                //std::cout << "antenna1 : (" << antenna1.r << "," << antenna1.c << ")";
+                                //std::cout << " antenna2 : (" << antenna2.r << "," << antenna2.c << ")";
+                                //std::cout << " point : (" << r << "," << c << ")";
+                                //std::cout << "\n";
+                                result.emplace(LocationPair(r,c));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result.size();
+    }
+}
+
+#endif //AOC2024_DAY08_HPP
