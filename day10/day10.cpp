97191d66066d499553be44ed76812f4c95409ba7
Chaitanya Kukde <ckukde@gmail.com>
2024-12-26 19:20:50 +0000

    Day 10 Parts 1 and 2

--- day10/day10.cpp
+++ day10/day10.cpp
@@ -2,4 +2,132 @@
 // Created by chaku on 25/12/24.
 //
 
+#include <vector>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
+#include <cassert>
+
 #include "day10.hpp"
+
+auto test() -> int {
+    const std::vector<std::string> test_input1 = {
+            "...0...",
+            "...1...",
+            "...2...",
+            "6543456",
+            "7.....7",
+            "8.....8",
+            "9.....9"
+    };
+    //std::cout << aoc::day10::sum_of_trailheads(test_input1) << "\n";
+    assert(aoc::day10::sum_of_trailheads(test_input1) == 2);
+
+    const std::vector<std::string> test_input2 = {
+            "..90..9",
+            "...1.98",
+            "...2..7",
+            "6543456",
+            "765.987",
+            "876....",
+            "987...."
+    };
+    assert(aoc::day10::sum_of_trailheads(test_input2) == 4);
+
+    const std::vector<std::string> test_input3 = {
+            "10..9..",
+            "2...8..",
+            "3...7..",
+            "4567654",
+            "...8..3",
+            "...9..2",
+            ".....01"
+    };
+    assert(aoc::day10::sum_of_trailheads(test_input3) == 3);
+
+    const std::vector<std::string> test_input4 = {
+            "89010123",
+            "78121874",
+            "87430965",
+            "96549874",
+            "45678903",
+            "32019012",
+            "01329801",
+            "10456732"
+    };
+    assert(aoc::day10::sum_of_trailheads(test_input4) == 36);
+
+    const std::vector<std::string> test_input5 = {
+            ".....0.",
+            "..4321.",
+            "..5..2.",
+            "..6543.",
+            "..7..4.",
+            "..8765.",
+            "..9...."
+    };
+    assert(aoc::day10::sum_of_ratings(test_input5) == 3);
+
+    const std::vector<std::string> test_input6 = {
+            "..90..9",
+            "...1.98",
+            "...2..7",
+            "6543456",
+            "765.987",
+            "876....",
+            "987...."
+    };
+    assert(aoc::day10::sum_of_ratings(test_input6) == 13);
+
+    const std::vector<std::string> test_input7 = {
+            "012345",
+            "123456",
+            "234567",
+            "345678",
+            "4.6789",
+            "56789."
+    };
+    assert(aoc::day10::sum_of_ratings(test_input7) == 227);
+
+    const std::vector<std::string> test_input8 = {
+            "89010123",
+            "78121874",
+            "87430965",
+            "96549874",
+            "45678903",
+            "32019012",
+            "01329801",
+            "10456732"
+    };
+    assert(aoc::day10::sum_of_ratings(test_input8) == 81);
+
+    return 0;
+}
+
+auto parse_and_run(std::string_view path) -> int {
+    std::vector<std::string> data;
+    std::fstream file(static_cast<std::string>(path));
+    if (!file.is_open()) {
+        std::cerr << "Failed to open " << std::quoted(path) << "\n";
+        return 1;
+    }
+    std::vector<std::string> input;
+    std::string line;
+    while(std::getline(file, line)) {
+        input.emplace_back(line);
+    }
+    std::cout << "Part 1 solution : " << aoc::day10::sum_of_trailheads(input) << "\n";
+    std::cout << "Part 2 solution : " << aoc::day10::sum_of_ratings(input) << "\n";
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc == 1) {
+        return test();
+    }
+    else if (argc == 2) {
+        return parse_and_run(argv[1]);
+    }
+    return 0;
+}
--- day10/day10.hpp
+++ day10/day10.hpp
@@ -5,4 +5,112 @@
 #ifndef AOC2024_DAY10_HPP
 #define AOC2024_DAY10_HPP
 
+#include <iostream>
+#include <cstdint>
+#include <set>
+#include <deque>
+#include <stack>
+
+namespace aoc::day10 {
+    [[maybe_unused]] auto print(const auto &container) {
+        size_t index{0};
+        const size_t sz{container.size()};
+        std::for_each(container.cbegin(), container.cend(), [&](const auto &elem) {
+            std::cout << elem;
+            if (index != sz - 1) {
+                std::cout << ", ";
+                index++;
+            }
+        });
+        std::cout << "\n";
+    };
+
+
+    using ui64 = uint64_t;
+    void check_part1(const std::vector<std::string> &topographic_map, const size_t num_rows, const size_t num_cols,
+                     std::pair<int, int> &&to_be_visited, std::set<std::pair<int, int>> &found_nines, ui64 &result) {
+        const auto &[row, col] = to_be_visited;
+        int curr_value = topographic_map[row][col] - '0';
+        if (curr_value == 9) {
+            if (!found_nines.contains(std::pair(row, col))) {
+                found_nines.emplace(row, col);
+                result += 1;
+                return;
+            }
+        }
+        if ((row + 1) < num_rows && topographic_map[row + 1][col] - '0' == curr_value + 1) {
+            check_part1(topographic_map, num_rows, num_cols, std::make_pair(row + 1, col), found_nines, result);
+        }
+        if ((row - 1) >= 0 && topographic_map[row - 1][col] - '0' == curr_value + 1) {
+            check_part1(topographic_map, num_rows, num_cols, std::make_pair(row - 1, col), found_nines, result);
+        }
+        if ((col + 1) < num_cols && topographic_map[row][col + 1] - '0' == curr_value + 1) {
+            check_part1(topographic_map, num_rows, num_cols, std::make_pair(row, col + 1), found_nines, result);
+        }
+        if ((col - 1) >= 0 && topographic_map[row][col - 1] - '0' == curr_value + 1) {
+            check_part1(topographic_map, num_rows, num_cols, std::make_pair(row, col - 1), found_nines, result);
+        }
+    }
+
+    auto sum_of_trailheads(const auto &topographic_map) -> uint64_t {
+        uint64_t result{0};
+
+        const auto num_rows = topographic_map.size();
+        const auto num_cols = topographic_map[0].size();
+
+        for (int r = 0; r < num_rows; ++r) {
+            for (int c = 0; c < num_cols; ++c) {
+                if (topographic_map[r][c] == '0') {
+                    uint64_t local = 0;
+                    std::set<std::pair<int, int>> found;
+                    check_part1(topographic_map, num_rows, num_cols, std::make_pair(r, c), found, local);
+                    result += local;
+                }
+            }
+        }
+        return result;
+    }
+
+    void check_part2(const std::vector<std::string> &topographic_map, const size_t num_rows, const size_t num_cols,
+                     std::pair<int, int> &&to_be_visited, std::set<std::pair<int, int>> &found_nines, ui64 &result) {
+        const auto &[row, col] = to_be_visited;
+        int curr_value = topographic_map[row][col] - '0';
+        if (curr_value == 9) {
+            result += 1;
+            return;
+        }
+        if ((row + 1) < num_rows && topographic_map[row + 1][col] - '0' == curr_value + 1) {
+            check_part2(topographic_map, num_rows, num_cols, std::make_pair(row + 1, col), found_nines, result);
+        }
+        if ((row - 1) >= 0 && topographic_map[row - 1][col] - '0' == curr_value + 1) {
+            check_part2(topographic_map, num_rows, num_cols, std::make_pair(row - 1, col), found_nines, result);
+        }
+        if ((col + 1) < num_cols && topographic_map[row][col + 1] - '0' == curr_value + 1) {
+            check_part2(topographic_map, num_rows, num_cols, std::make_pair(row, col + 1), found_nines, result);
+        }
+        if ((col - 1) >= 0 && topographic_map[row][col - 1] - '0' == curr_value + 1) {
+            check_part2(topographic_map, num_rows, num_cols, std::make_pair(row, col - 1), found_nines, result);
+        }
+    }
+
+    auto sum_of_ratings(const auto &topographic_map) -> uint64_t {
+        uint64_t result{0};
+
+        const auto num_rows = topographic_map.size();
+        const auto num_cols = topographic_map[0].size();
+
+        for (int r = 0; r < num_rows; ++r) {
+            for (int c = 0; c < num_cols; ++c) {
+                if (topographic_map[r][c] == '0') {
+                    uint64_t local = 0;
+                    std::set<std::pair<int, int>> found;
+                    check_part2(topographic_map, num_rows, num_cols, std::make_pair(r, c), found, local);
+                    result += local;
+                }
+            }
+        }
+        return result;
+    }
+}
+
 #endif //AOC2024_DAY10_HPP
